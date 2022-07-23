d68e217550c915691c65c460f8eafe93cf0c8618
Piyush Srivastava <67270567+piyush-sri@users.noreply.github.com>
2022-07-23 23:51:48 +0530

    oneliner website

---
+++ onliner.js
@@ -0,0 +1,233 @@
+$(document).ready(function(){
+  $("#h").click(function(){
+      $("#a").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#a").hide();
+  $("#sw").click(function(){
+     $("#a").show(1000);
+  });
+
+  $("#hh").click(function(){
+      $("#b").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#b").hide();
+  $("#ssw").click(function(){
+     $("#b").show(1000);
+  });  
+  
+    $("#hhh").click(function(){
+      $("#c").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#c").hide();
+  $("#sssw").click(function(){
+     $("#c").show(1000);
+  });
+  
+   $("#aa").click(function(){
+      $("#d").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#d").hide();
+  $("#bb").click(function(){
+     $("#d").show(1000);
+  });
+  
+    $("#aaa").click(function(){
+      $("#e").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#e").hide();
+  $("#bbb").click(function(){
+     $("#e").show(1000);
+  });
+  
+    $("#aaaa").click(function(){
+      $("#f").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#f").hide();
+  $("#bbbb").click(function(){
+     $("#f").show(1000);
+  });
+  //question no.7
+   $("#ab").click(function(){
+      $("#g").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#g").hide();
+  $("#bc").click(function(){
+     $("#g").show(1000);
+  });
+  //question number 8
+  $("#abc").click(function(){
+      $("#gg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#gg").hide();
+  $("#bca").click(function(){
+     $("#gg").show(1000);
+  });
+  
+  //question no. 9
+   $("#abcc").click(function(){
+      $("#ggg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#ggg").hide();
+  $("#bcaa").click(function(){
+     $("#ggg").show(1000);
+  });
+  
+  //question no. 10
+   $("#abccc").click(function(){
+      $("#gggg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#gggg").hide();
+  $("#bcaaa").click(function(){
+     $("#gggg").show(1000);
+  });
+  //question no. 11
+   $("#ca").click(function(){
+      $("#gf").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#gf").hide();
+  $("#ac").click(function(){
+     $("#gf").show(1000);
+  });
+  
+  //question no. 12
+    $("#caa").click(function(){
+      $("#gff").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#gff").hide();
+  $("#acc").click(function(){
+     $("#gff").show(1000);
+  });
+  
+  //question no. 13
+   $("#caaa").click(function(){
+      $("#gfff").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#gfff").hide();
+  $("#accc").click(function(){
+     $("#gfff").show(1000);
+  });
+  
+  //question no. 14
+    $("#cb").click(function(){
+      $("#fg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#fg").hide();
+  $("#mc").click(function(){
+     $("#fg").show(1000);
+  });
+  
+  //question no.15
+    $("#cbb").click(function(){
+      $("#fgg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#fgg").hide();
+  $("#mcc").click(function(){
+     $("#fgg").show(1000);
+  });
+  
+  //question no. 16
+  $("#cbbb").click(function(){
+      $("#fggg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#fggg").hide();
+  $("#mccc").click(function(){
+     $("#fggg").show(1000);
+  });
+  
+  //question no. 17
+   $("#ccbb").click(function(){
+      $("#ffgg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#ffgg").hide();
+  $("#mmc").click(function(){
+     $("#ffgg").show(1000);
+  });
+  
+  //question no.18
+    $("#ccb").click(function(){
+      $("#fffg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#fffg").hide();
+  $("#mmm").click(function(){
+     $("#fffg").show(1000);
+  });
+  
+  //question no.19
+   $("#cccb").click(function(){
+      $("#ffffg").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#ffffg").hide();
+  $("#mmmb").click(function(){
+     $("#ffffg").show(1000);
+  });
+  
+  //question no. 20
+    $("#ccccb").click(function(){
+      $("#fffff").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#fffff").hide();
+  $("#mmmmb").click(function(){
+     $("#fffff").show(1000);
+  });
+  
+  //question no. 21
+  $("#cbccb").click(function(){
+      $("#cci").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#cci").hide();
+  $("#mbmmb").click(function(){
+     $("#cci").show(1000);
+  });
+  
+  //question no. 22
+   $("#db").click(function(){
+      $("#jk").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#jk").hide();
+  $("#nm").click(function(){
+     $("#jk").show(1000);
+  });
+  
+  //question no. 23
+  $("#dbb").click(function(){
+      $("#jkk").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#jkk").hide();
+  $("#nmm").click(function(){
+     $("#jkk").show(1000);
+  });
+  
+  //question no. 24
+   $("#ddb").click(function(){
+      $("#jkkk").hide(1000);
+	  });
+	  //hide an element in webpage
+	  $("#jkkk").hide();
+  $("#nmv").click(function(){
+     $("#jkkk").show(1000);
+  });
+ });
+
